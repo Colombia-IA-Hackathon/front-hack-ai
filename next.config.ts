c53cd54f54c2352352463eae60481e8e7ba9a707
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-23 13:30:10 -0500

    Add environment variable configuration for Mapbox API key

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	env: {
+		MAP_BOX_API_KEY: process.env.MAP_BOX_API_KEY,
+	},
 };
 
 export default nextConfig;
