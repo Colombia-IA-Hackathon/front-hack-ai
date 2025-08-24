0946b732d64d93bf1bb656a9e84feecd8ecd52f3
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-24 04:29:31 -0500

    Refactor page.tsx: comment out unused imports and code for clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,33 @@
 "use client";
-import { useInsurance } from "@/hooks/insurance";
-import { Hex } from "viem";
+// import { useInsurance } from "@/hooks/insurance";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+// import { Hex } from "viem";
+// import { useWalletClient } from "wagmi";
 
 export default function Home() {
-  const { createInsuranceClone: createInsuranceCloneHook } = useInsurance();
-  const { mutate: createInsuranceClone, isPending: isCreatingInsuranceClone } =
-    createInsuranceCloneHook;
+	// const { data: walletClient } = useWalletClient();
+	// const { createInsuranceClone: createInsuranceCloneHook } = useInsurance(walletClient);
+	// const { mutate: createInsuranceClone, isPending: isCreatingInsuranceClone } = createInsuranceCloneHook;
 
-  const onCreateInsuranceClone = async () => {
-    createInsuranceClone(undefined, {
-      onSuccess: (hash: Hex) => {
-        // Handle success
-        console.log("hash:", hash);
-      },
-      onError: (error: unknown) => {
-        // Handle error
-        console.error("Error:", error);
-      },
-    });
-  };
+	// const onCreateInsuranceClone = async () => {
+	// 	createInsuranceClone(undefined, {
+	// 		onSuccess: (hash: Hex) => {
+	// 			console.log("hash:", hash);
+	// 		},
+	// 		onError: (error: unknown) => {
+	// 			// Handle error
+	// 			console.error("Error:", error);
+	// 		},
+	// 	});
+	// };
 
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h2>hola mundo</h2>
-      <button
-        onClick={onCreateInsuranceClone}
-        disabled={isCreatingInsuranceClone}
-      >
-        {isCreatingInsuranceClone ? "Creating..." : "Create Insurance Clone"}
-      </button>
-    </div>
-  );
+	return (
+		<div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
+			<h2>hola mundo</h2>
+			{/* <button onClick={onCreateInsuranceClone} disabled={isCreatingInsuranceClone}>
+				{isCreatingInsuranceClone ? "Creating..." : "Create Insurance Clone"}
+			</button> */}
+			<ConnectButton />;
+		</div>
+	);
 }
