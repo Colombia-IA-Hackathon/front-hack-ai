cd34a84709de9339524294e97ce55678b2ebda5b
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-23 20:50:38 -0500

    Refactor MapView component: simplify chat visibility logic and enhance layout with view transition

--- src/app/(pages)/dashboard/map/page.tsx
+++ src/app/(pages)/dashboard/map/page.tsx
@@ -1,32 +1,28 @@
 "use client";
 import MapProvider from "@/app/components/mapbox";
-import { useState } from "react";
 import Chat from "@/app/components/chat";
+import { unstable_ViewTransition as ViewTransition } from "react";
 
 const MapView = () => {
-	const [isOpenChat, setIsOpenChat] = useState(false);
+	// const { setCoordinates } = useGeo();
+	// const [isOpenChat, setIsOpenChat] = useState(false);
 
 	return (
-		<div className='container mx-auto p-4 h-[calc(100vh-80px)]'>
-			<div className={`grid ${isOpenChat ? "md:grid-cols-2" : "grid-cols-1"} gap-5 h-full`}>
+		<div className={`container mx-auto p-4 transition-all duration-300`}>
+			<div className={`grid lg:grid-cols-2 grid-cols-1 gap-5 h-full`}>
 				<MapProvider
 					initialViewState={{
 						longitude: -74.2973,
 						latitude: 4.5709,
-						zoom: 5,
+						zoom: 1,
 					}}
-					onMapClick={({ lng, lat }) => {
-						setIsOpenChat(true);
-						console.log("Latitud:", lat, "Longitud:", lng);
-					}}
-					// className="h-[50vh] md:h-full"
 				/>
 
-				{isOpenChat && (
-					<div className='h-[50vh] md:h-full'>
+				<ViewTransition name='chat-page'>
+					<div className={`h-[50vh] md:h-full transform transition-all duration-300`}>
 						<Chat />
 					</div>
-				)}
+				</ViewTransition>
 			</div>
 		</div>
 	);
