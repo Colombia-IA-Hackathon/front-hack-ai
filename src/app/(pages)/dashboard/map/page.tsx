e8468fa01845780134396661bfec50ada817f128
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-24 01:07:29 -0500

    Refactor MapView component: use useMemo for initial view state and clean up code

--- src/app/(pages)/dashboard/map/page.tsx
+++ src/app/(pages)/dashboard/map/page.tsx
@@ -2,24 +2,24 @@
 import MapProvider from "@/app/components/mapbox";
 import Chat from "@/app/components/chat";
 import { unstable_ViewTransition as ViewTransition } from "react";
+import { useMemo } from "react";
 
 const MapView = () => {
-	// const { setCoordinates } = useGeo();
-	// const [isOpenChat, setIsOpenChat] = useState(false);
+	const initialViewState = useMemo(
+		() => ({
+			longitude: -74.2973,
+			latitude: 4.5709,
+			zoom: 1,
+		}),
+		[]
+	);
 
 	return (
-		<div className={`container mx-auto p-4 transition-all duration-300`}>
-			<div className={`grid lg:grid-cols-2 grid-cols-1 gap-5 h-full`}>
-				<MapProvider
-					initialViewState={{
-						longitude: -74.2973,
-						latitude: 4.5709,
-						zoom: 1,
-					}}
-				/>
-
+		<div className='container mx-auto p-4 transition-all duration-300'>
+			<div className='grid lg:grid-cols-2 grid-cols-1 gap-5 h-full'>
+				<MapProvider initialViewState={initialViewState} />
 				<ViewTransition name='chat-page'>
-					<div className={`h-[50vh] md:h-full transform transition-all duration-300`}>
+					<div className='h-[50vh] md:h-full transform transition-all duration-300'>
 						<Chat />
 					</div>
 				</ViewTransition>
@@ -27,4 +27,5 @@ const MapView = () => {
 		</div>
 	);
 };
+
 export default MapView;
