7579b081af70e5c480b1adc8268f701667db4e6c
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-23 20:50:45 -0500

    Enhance MapProvider component: integrate Mapbox Geocoder, manage markers on map click, and improve flyTo functionality

--- src/app/components/mapbox/index.tsx
+++ src/app/components/mapbox/index.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import { useOpenChat } from "@/app/store/useOpenChat";
+import { useGeo } from "@/app/store/useGeo";
 
 // Extend context to expose flyTo helper
 import { MapContext } from "./mapbox-context";
@@ -19,13 +23,21 @@ type MapComponentProps = {
 	children?: React.ReactNode;
 };
 
-const flyTo = (map: mapboxgl.Map | null, lng: number, lat: number, zoom: number = 14) => {
-	if (map) {
-		map.flyTo({ center: [lng, lat], zoom });
-	}
-};
-
 export default function MapProvider({ initialViewState, onMapClick, children }: MapComponentProps) {
+	const flyTo = (map: mapboxgl.Map | null, lng: number, lat: number, zoom: number = 14) => {
+		if (map) {
+			setCoordinates(lat, lng);
+			// openChat();
+			map.flyTo({ center: [lng, lat], zoom });
+		}
+	};
+	const addMarker = (map: mapboxgl.Map | null, lng: number, lat: number) => {
+		if (map) {
+			new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
+		}
+	};
+
+	const { setCoordinates } = useGeo();
 	const mapContainerRef = useRef<HTMLDivElement>(null);
 	const map = useRef<mapboxgl.Map | null>(null);
 	const [loaded, setLoaded] = useState(false);
@@ -41,6 +53,12 @@ export default function MapProvider({ initialViewState, onMapClick, children }: 
 			zoom: initialViewState.zoom,
 			attributionControl: false,
 			logoPosition: "bottom-right",
+
+			// add animmation to roate globe
+			antialias: true,
+			pitchWithRotate: false,
+			dragRotate: false,
+			renderWorldCopies: false,
 		});
 
 		map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
@@ -50,9 +68,28 @@ export default function MapProvider({ initialViewState, onMapClick, children }: 
 			setLoaded(true);
 		});
 
+		const geocoder = new MapboxGeocoder({
+			accessToken: mapboxgl.accessToken as string,
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			mapboxgl: mapboxgl as any,
+		});
+
+		map.current.addControl(geocoder, "top-left");
+
 		map.current.on("click", (e) => {
 			flyTo(map.current, e.lngLat.lng, e.lngLat.lat);
+			// Remove previous marker if exists
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			if (map.current && (map.current as any)._lastMarker) {
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				(map.current as any)._lastMarker.remove();
+			}
+			// Add new marker and store reference
+			const marker = new mapboxgl.Marker().setLngLat([e.lngLat.lng, e.lngLat.lat]).addTo(map.current!);
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			(map.current as any)._lastMarker = marker;
 			if (onMapClick) {
+				console.log("onMapClick defined");
 				onMapClick({ lng: e.lngLat.lng, lat: e.lngLat.lat });
 			}
 		});
