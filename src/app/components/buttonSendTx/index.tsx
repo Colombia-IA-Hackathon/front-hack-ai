9046a2f057776d14529d9ea70f7e645dc01bb3e6
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-24 04:29:43 -0500

    Add ButtonTX component: implement transaction creation with success and error notifications

---
+++ src/app/components/buttonSendTx/index.tsx
@@ -0,0 +1,28 @@
+import { useInsurance } from "@/hooks/insurance";
+import { Hex } from "viem";
+import { useWalletClient } from "wagmi";
+import toast from "react-hot-toast";
+
+const ButtonTX = () => {
+	const { data: walletClient } = useWalletClient();
+	const { createInsuranceClone: createInsuranceCloneHook } = useInsurance(walletClient);
+	const { mutate: createInsuranceClone, isPending: isCreatingInsuranceClone } = createInsuranceCloneHook;
+	const onCreateInsuranceClone = async () => {
+		createInsuranceClone(undefined, {
+			onSuccess: (hash: Hex) => {
+				toast.success(`Transaction sent: ${hash}`);
+			},
+			onError: (error: unknown) => {
+				toast.error(`Error: ${error}`);
+			},
+		});
+	};
+
+	return (
+		<button onClick={onCreateInsuranceClone} disabled={isCreatingInsuranceClone} className='btn btn-secondary rounded-2xl'>
+			{isCreatingInsuranceClone ? "Creating..." : "Crear Seguro"}
+		</button>
+	);
+};
+
+export default ButtonTX;
