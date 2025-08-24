0a3f4b1e26d68b39d56770dfd4ce9369f8db58c4
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-24 04:29:35 -0500

    Refactor QueryProvider.tsx: integrate WagmiProvider and RainbowKitProvider for enhanced functionality

--- src/app/QueryProvider.tsx
+++ src/app/QueryProvider.tsx
@@ -1,11 +1,29 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { sepolia } from "wagmi/chains";
+
 const queryClient = new QueryClient();
 interface QueryProviderProps {
 	children: React.ReactNode;
 }
 
+const config = getDefaultConfig({
+	appName: "Insure AI",
+	projectId: process.env.PROJECT_ID as string,
+	chains: [sepolia],
+	ssr: true,
+});
+
 export default function QueryProvider({ children }: QueryProviderProps) {
-	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+	return (
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={queryClient}>
+				<RainbowKitProvider>{children}</RainbowKitProvider>
+			</QueryClientProvider>
+		</WagmiProvider>
+	);
 }
